4617e00d66d9b70d5b03fbad83192d9b3c371b89
19020344 Phạm Hoàng Lâm <19020344@vnu.edu.vn>
2022-05-24 16:13:11 +0700

    go letterrrr!!

--- stream-chat-android-ui-components-sample/src/demo/kotlin/io/getstream/chat/ui/sample/application/AppConfig.kt
+++ stream-chat-android-ui-components-sample/src/demo/kotlin/io/getstream/chat/ui/sample/application/AppConfig.kt
@@ -50,7 +50,7 @@ object AppConfig {
             apiKey = apiKey,
             id = "truccute",
             name = "Thanh Truc",
-            image = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/e97a620e-d4a3-4e98-9e62-2b4a19b92e97/dbp7d3o-13407102-3240-4139-911b-4c7cb161b34b.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2U5N2E2MjBlLWQ0YTMtNGU5OC05ZTYyLTJiNGExOWI5MmU5N1wvZGJwN2Qzby0xMzQwNzEwMi0zMjQwLTQxMzktOTExYi00YzdjYjE2MWIzNGIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.9Tq3Ni8Snb2HhQW0CRyOXHfQDiNZrx-Usk5gMEgs31w",
+            image = "https://scontent.fhan14-1.fna.fbcdn.net/v/t39.30808-1/282624888_1174208366755579_9130817381715712466_n.jpg?stp=c4.0.320.320a_dst-jpg_p320x320&_nc_cat=101&ccb=1-7&_nc_sid=7206a8&_nc_ohc=uCEudmMaB1kAX-_rnwq&tn=iTi3PNQ4jEFFARIq&_nc_ht=scontent.fhan14-1.fna&oh=00_AT_hIG_f9voNSsbygWw9SOJl9cF_vIVMZpBPvuky4o4YJw&oe=629168B1",
             token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidHJ1Y2N1dGUifQ.F8UjdWRkqh3eWrMh3eFmri0Pq5gGvmAL7IE8fVBeFp4"
         ),
         SampleUser(
